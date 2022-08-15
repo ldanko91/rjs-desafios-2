945c08e06cb0267403e826856d24f68a3d5128ca
Leandro Ezequiel Danko <100171997+ldanko91@users.noreply.github.com>
2022-08-15 20:14:20 -0300

    coder-rjs-desafio-1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Estoy aprendiendo ReactJS @<a href="http://www.coderhouse.com">CoderHouse</a>
         </a>
       </header>
     </div>
