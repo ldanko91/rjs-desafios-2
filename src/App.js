6d6fc2d1708de87f33cc38c0fdadde2cc887a82d
Leandro Ezequiel Danko <100171997+ldanko91@users.noreply.github.com>
2022-08-17 16:11:21 -0300

    coder-rjs-desafio-2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/navbar/Navbar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Estoy aprendiendo React JS @<a href="http://www.coderhouse.com">CoderHouse</a>
-        </a>
+       <Navbar></Navbar> 
       </header>
     </div>
   );
