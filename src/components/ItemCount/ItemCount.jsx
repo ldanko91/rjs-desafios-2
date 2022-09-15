2a5afae3697999e0459a3fbf27413473b4c572f7
Leandro Ezequiel Danko <100171997+ldanko91@users.noreply.github.com>
2022-09-15 12:53:11 -0300

    coder-rjs-desafio-7-reentrega-1

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -23,7 +23,7 @@ const ItemCount = ({stock, initial, onAdd}) => {
             <Button variant="secondary" onClick={adItem}>+</Button>
             <p class='Recuento'>{itemCount}</p>
             <Button variant="secondary" onClick={subItem}>-</Button> <br></br>
-            <Button variant="primary" onClick={()=>{onAdd(itemCount)}}>Añadir al carrito!</Button>
+            <Button variant="primary" onClick={()=>{onAdd(true)}}>Añadir al carrito!</Button>
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,31 +1,40 @@
 import './ItemDetail.css'
 import ItemCount from '../ItemCount/ItemCount'
 import { useState, useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
 
 
 const ItemDetail = ({detalle}) => {
     const {addProduct} = useContext(CartContext);
-    // const [cuenta , setCuenta] = useState (0)
-    const onAdd = (contador) => {
-        // setCuenta(contador)
-        console.log(contador)
-        // console.log(`Has añadido ${cuenta} unidades al carrito`)
-        const newProduct = {...detalle, qty: contador}
-        console.log('newProduct', newProduct)
-        addProduct(newProduct)
-    }
     const data = detalle
-
-        return (
-            <div className="item-detail">
+    const [onAdd, setOnAdd] = useState(false);
+        if (onAdd) {
+            return (
+                <div className="item-detail">
                     <img src={data.img} alt={data.nombre} />
                     <h3>Precio ${data.precio}.-</h3>
-                    <ItemCount stock={data.stock} initial={1} onAdd={onAdd}/>   
-            </div>
+                    <Link to='/cart'><button>Finalizar compra!</button></Link>
+                </div>
             )
-
+        } else {
+            return (
+                <div className="item-detail">
+                    <img src={data.img} alt={data.nombre} />
+                    <h3>Precio ${data.precio}.-</h3>
+                    <ItemCount stock={data.stock} initial={1} onAdd={setOnAdd}/>   
+                </div>
+            )
+        }
 }
 
+
 export default ItemDetail
+
+
+// // setCuenta(contador)
+// console.log(contador)
+// // console.log(`Has añadido ${cuenta} unidades al carrito`)
+// const newProduct = {...detalle, qty: contador}
+// console.log('newProduct', newProduct)
+// addProduct(newProduct)
